f3b3b3dcd71e4a19f0722f7b6f80dd4961ad85e2
Muhammad Afif Fauzi <afiffauzi234@gmail.com>
2022-07-13 06:10:19 +0700

    add connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const notesRouter = require("./routes/notes");
 
 // express server
@@ -9,6 +10,7 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
@@ -17,7 +19,24 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/notes", notesRouter);
 
-// listen on port 4000
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port`, process.env.PORT);
+// error handlers
+app.use("/", (req, res, next) => {
+  res.status(404);
+  res.json({
+    status: "404",
+    errors: "page not found",
+  });
 });
+
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    // listen on port 4000
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to database & listening on port`, process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
