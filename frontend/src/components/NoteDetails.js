49c1a54b11a3b2b24660be108477f61efa0c6014
Muhammad Afif Fauzi <afiffauzi234@gmail.com>
2022-08-20 18:41:04 +0700

    add authorization headers

--- frontend/src/components/NoteDetails.js
+++ frontend/src/components/NoteDetails.js
@@ -1,13 +1,20 @@
 import { useNotesContext } from "../hooks/useNotesContext";
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
-
+import { useAuthContext } from "../hooks/useAuthContext";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 const NoteDetails = ({ note }) => {
+  const { user } = useAuthContext();
   const { dispatch } = useNotesContext();
 
   const handleDelete = async () => {
+    if (!user) {
+      return;
+    }
     const response = await fetch("/api/notes/" + note._id, {
-      method: "DELETE"
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
     });
     const json = await response.json();
 
@@ -19,12 +26,16 @@ const NoteDetails = ({ note }) => {
   return (
     <div className="note-details">
       <h4>{note.title}</h4>
-      <h2><strong>{note.body}</strong></h2>
+      <h2>
+        <strong>{note.body}</strong>
+      </h2>
       <p>{formatDistanceToNow(new Date(note.createdAt), { addSuffix: true })}</p>
       <span className="material-symbols-outlined">edit</span>
-      <span className="material-symbols-outlined" onClick={handleDelete}>delete</span>
+      <span className="material-symbols-outlined" onClick={handleDelete}>
+        delete
+      </span>
     </div>
   );
 };
 
-export default NoteDetails 
+export default NoteDetails;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,32 +1,36 @@
 import { useEffect } from "react";
 import { useNotesContext } from "../hooks/useNotesContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 import NoteDetails from "../components/NoteDetails";
 import NoteForm from "../components/NoteForm";
 
 const Home = () => {
   const { notes, dispatch } = useNotesContext();
-  
+  const { user } = useAuthContext();
+
   useEffect(() => {
     const fetchNotes = async () => {
-      const response = await fetch("/api/notes");
+      const response = await fetch("/api/notes", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
       const json = await response.json();
-      
+
       if (response.ok) {
-        dispatch({ type: "SET_NOTES", payload: json })
+        dispatch({ type: "SET_NOTES", payload: json });
       }
     };
-    
-    fetchNotes();
-  }, [dispatch]);
-  
+
+    if (user) {
+      fetchNotes();
+    }
+  }, [dispatch, user]);
+
   return (
     <div className="home">
-      <div className="notes">
-        {notes && notes.map((note) => (
-          <NoteDetails key={note._id} note={note} />
-        ))}
-      </div>
+      <div className="notes">{notes && notes.map((note) => <NoteDetails key={note._id} note={note} />)}</div>
       <NoteForm />
     </div>
   );
