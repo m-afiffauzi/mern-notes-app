b7549f9b0ef806a5e9e2818e5684c496fe1e6aa9
Muhammad Afif Fauzi <afiffauzi234@gmail.com>
2022-12-31 22:49:03 +0700

    add vercel config

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const mongoose = require("mongoose");
-const notesRouter = require("./routes/notes");
-const usersRouter = require("./routes/users");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const notesRouter = require('./routes/notes');
+const usersRouter = require('./routes/users');
 
 // express server
 const app = express();
@@ -18,15 +19,21 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use("/api/notes", notesRouter);
-app.use("/api/users", usersRouter);
+app.use('/api/notes', notesRouter);
+app.use('/api/users', usersRouter);
+
+// static files (frontend build)
+app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+});
 
 // error handlers
-app.use("/", (req, res, next) => {
+app.use('/', (req, res, next) => {
   res.status(404);
   res.json({
-    status: "404",
-    errors: "page not found",
+    status: '404',
+    errors: 'page not found',
   });
 });
 
